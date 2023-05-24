b7d5006342520d75b4d044868680b08e46c34919
AyberkHalac <AyberkHalac@users.noreply.github.com>
2023-05-24 17:22:53 +0300

    Pagination is added to the find_circular_trust.py
    The role gathering part was limited, so I added pagination to it.

--- find_circular_trust.py
+++ find_circular_trust.py
@@ -8,6 +8,28 @@ import networkx as nx
 from collections import defaultdict
 
 
+def iam_list_roles():
+    iam_client = boto3.client('iam')
+    roles = []
+    response = {}
+    is_truncated = False
+
+    try:
+        while len(response.keys()) == 0 or is_truncated is True:
+            if is_truncated is False:
+                response = iam_client.list_roles()
+            else:
+                response = iam_client.list_roles(Marker=response['Marker'])
+
+            for role in response['Roles']:
+                roles.append(role)
+
+            is_truncated = response['IsTruncated']
+        return roles
+    except Exception as e:
+        raise
+
+
 def getCycles(aws_roles):
     g = nx.DiGraph()
     for jsonRole in aws_roles:
@@ -23,15 +45,16 @@ def getCycles(aws_roles):
                         dest = arn
                         g.add_edges_from([(source, dest)])
                 else:
-                    dest  =arns
+                    dest = arns
                     g.add_edges_from([(source, dest)])
-                
+
     cycles = list(nx.simple_cycles(g))
     return cycles
 
+
 if __name__ == "__main__":
     client = boto3.client('iam')
-    aws_roles = client.list_roles()['Roles']
+    aws_roles = client.iam_list_roles()
 
     cycles = getCycles(aws_roles)
     for c in cycles:
